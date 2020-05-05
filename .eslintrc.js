d04ddac454b3499ef483393d8dd43cdad1764d7f
hcrev <dan@Device-Primary.local>
2020-05-05 12:23:04 -0400

    Genesis commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "rules": {
+        'no-console': 0,
+        'no-undef': 2,
+        "no-var": 2
+    },
+    "globals": {
+        "$": true,
+        "Macy": true,
+        "module": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    }
+};
